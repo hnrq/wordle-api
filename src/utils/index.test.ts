32154043d9a7e72fb4b12b8d65592bd6ecb1b1d6
Henrique Ramos <henriquealberone@outlook.com>
2022-04-08 02:04:25 -0300

    Fix utils tests

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,24 +1,4 @@
-import { letterStatus, compareWords, countLetters } from './index';
-
-describe('letterStatus()', () => {
-  it("returns 'correct' if target word contains the letter at the given index \
-  and letter count is bigger than 0", () => {
-    expect(letterStatus('words', 'o', 1, 1)).toBe('correct');
-  });
-
-  it("returns 'present' if target word contains the letter but not at the given index \
-   and letter count is bigger than 0", () => {
-    expect(letterStatus('words', 'o', 2, 1)).toBe('present');
-  });
-
-  it("returns 'absent' if target word does not contain the letter", () => {
-    expect(letterStatus('words', 'a', 2, 1)).toBe('absent');
-  });
-
-  it("returns 'absent' if target word contains the letter but letter count is equals 0", () => {
-    expect(letterStatus('words', 'o', 4, 0)).toBe('absent');
-  });
-});
+import { compareWords, getAvailableLettersPool } from './index';
 
 describe('compareWords()', () => {
   it("Returns 'absent' for letters duplicated at guess but not at target", () => {
@@ -26,13 +6,19 @@ describe('compareWords()', () => {
   });
 });
 
-describe('countLetters()', () => {
-  it('Counts the appearances of letters in a given word', () => {
-    expect(countLetters('atoll')).toEqual({
+describe('getAvailableLettersPool()', () => {
+  it('Counts present and absent letters on the guess', () => {
+    const answer = 'total';
+    const guess = 'atoll';
+
+    const letterPairs: Array<[string, string]> = Array.from(answer, (_, i) => [
+      answer[i],
+      guess[i]
+    ]);
+    expect(getAvailableLettersPool(letterPairs)).toEqual({
       a: 1,
-      t: 1,
       o: 1,
-      l: 2
+      t: 2
     });
   });
 });
